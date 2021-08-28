2a6f190c7240464a4346bd2f362f214d092f79d6
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2021-08-28 19:05:51 +0000

    Clicked btn can now display on the calculator screen

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
 let cal_screen = document.getElementById("screen");
-
-
+let equal_btn = document.querySelector(".equal-btn");
+let clear_screen_btn = document.querySelector(".cancel-btn");
 let cal_btn = document.getElementsByClassName("btn");
-console.log(cal_btn);
 
-for(let i = 0; i < cal_btn.length; i++){
-    // console.log(cal_btn[i].innerHTML);
-    cal_btn[i].addEventListener("click", displayElement);
+// display element on the calculator screen when a btn is clicked
+function displayClickedElement(){
+    for(let i = 0; i < cal_btn.length; i++){
+        // console.log(cal_btn[i].innerHTML);
+        cal_btn[i].addEventListener("click", (e)=>{
+            let clicked_btn = e.target.innerText;
+            cal_screen.value += clicked_btn;
+            
+            console.log(clicked_btn);
+        });
+    }
+    
 }
 
-// display element
-function displayElement(e){
-    cal_btn.innerHTML = e.target.value;
-    console.log(cal_screen);
+// execute calculation base upon the user input 
+function executeCalculation(){
+    if(cal_screen.value !== ""){
+        console.log("screen is not empty");
+
+        let first_num;
+        let second_num;
+        let operator;
+
+        for(let i = 0; i < cal_screen.value.length; i++){
+            console.log(`value ${i} is now`, cal_screen.value[i]);
+        }
+    }
 }
+
+//reset the calculator screen to default
+function clearCalScreen(){
+    if(cal_screen.value !== ""){
+        cal_screen.value = "";
+    }
+}
+
+
+equal_btn.addEventListener("click", executeCalculation);
+clear_screen_btn.addEventListener("click", clearCalScreen);
+displayClickedElement();
