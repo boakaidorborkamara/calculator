9a06381b2047ab6139cc974d389f62c65077b920
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2021-09-01 16:47:36 +0000

    commit done after clear screen and clear digit functionality

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let cal_screen = document.getElementById("screen");
 let equal_btn = document.querySelector(".equal-btn");
 let clear_screen_btn = document.querySelector(".cancel-btn");
+let delete_btn = document.querySelector(".delete-btn");
 let cal_btn = document.getElementsByClassName("btn");
 
 // display element on the calculator screen when a btn is clicked
@@ -21,14 +22,22 @@ function displayClickedElement(){
 function executeCalculation(){
     if(cal_screen.value !== ""){
         console.log("screen is not empty");
+        console.log(typeof cal_screen.value);
 
-        let first_num;
-        let second_num;
-        let operator;
+        let result = parseInt(cal_screen.value);
+        console.log(result);
+        console.log(typeof result);
 
-        for(let i = 0; i < cal_screen.value.length; i++){
-            console.log(`value ${i} is now`, cal_screen.value[i]);
-        }
+        // let cal_operators = ["*", "+","x", ".", "-","%"];
+        // console.log(cal_operators);
+
+        // let first_num;
+        // let operator;
+        // let second_num;
+
+        // for(let i = 0; i < cal_screen.value.length; i++){
+        //     console.log(`value ${i} is now`, cal_screen.value[i]);
+        // }
     }
 }
 
@@ -39,7 +48,21 @@ function clearCalScreen(){
     }
 }
 
+//delete the first figure from the user input
+function deleteUserInput(){
+    if(cal_screen.value != "")
+    {
+        //value that the user just inputted
+        let user_current_value = cal_screen.value;
+
+        //removing the last value from user current value
+        let new_value = user_current_value.slice(0, -1);
+        cal_screen.value = new_value;
+    }
+}
+
 
 equal_btn.addEventListener("click", executeCalculation);
 clear_screen_btn.addEventListener("click", clearCalScreen);
+delete_btn.addEventListener("click", deleteUserInput);
 displayClickedElement();
